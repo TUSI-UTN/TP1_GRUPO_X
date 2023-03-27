fa1a78773956c5b123708a28a5ac22f912b22bdd
Agustin Carusillo <agus.carusilo@hotmail.com>
2023-03-27 21:45:26 +0200

    - New Class Genre added - Genre parameter added to other classes - Some progress on ChildrensTicket class

---
+++ src/entities/Genre.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.time.LocalDateTime;
+
+/* Clase genero
+ * Los generos posibles para los recitales son los siguientes: rock, heavy metal, reggaeton, trap, latinos y pop.
+ * Los generos de los teatros son los siguientes: drama, teatro o comedia.
+ */
+
+public class Genre {
+	
+	protected String name;
+	
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public String getNombre() {
+        return name;
+    }
+
+}
