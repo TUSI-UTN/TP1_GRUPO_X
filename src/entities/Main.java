066328cfecf06be7bc639dc82d92074dd26932db
Pablo Torres <p.torres0991@gmail.com>
2023-03-28 23:47:44 -0300

    Desarrollo de clase TheaterTicket

--- src/entities/Main.java
+++ src/entities/Main.java
@@ -1,9 +1,16 @@
 package entities;
 
+import java.io.Console;
+import java.time.LocalDateTime;
+import java.time.Month;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
-
+		Genre Drama = new Genre ("Drama");
+		
+	    LocalDateTime dateTime = LocalDateTime.of(2022, Month.MAY, 1, 19, 30);
+		TheaterTicket ticket = new TheaterTicket("obra1", dateTime, 120, Drama, 3);
+		System.out.println(ticket.toString());
+		}
 }
--- src/entities/TheaterTicket.java
+++ src/entities/TheaterTicket.java
@@ -3,32 +3,56 @@ package entities;
 import java.time.LocalDateTime;
 
 /**
- * Las entradas para el teatro tienen un valor fijo de 1350,50$. 
- * A su vez, los teatros tienen sus propios gï¿½neros y actores principales.
- * Consideraremos que como mï¿½ximo se puede tener 3 actores principales. 
- * Los gï¿½neros de los teatros son los siguientes: drama, teatro o comedia.
- * 
- * @author tscutti
- *
+ * Las entradas para el teatro tienen un valor fijo de 1350,50$.  A su vez, los teatros tienen sus propios generos y actores principales.
+ * Consideraremos que como maximo se puede tener 3 actores principales. Los generos de los teatros son los siguientes: drama, teatro o comedia.
  */
 public class TheaterTicket extends Ticket {
 	
 	private Genre genre;
+	private int actoresPrincipales;
 
-	public TheaterTicket(String name, LocalDateTime dateTime, int minutes, Genre genre) {
+	public TheaterTicket(String name, LocalDateTime dateTime, int minutes, Genre genre, int actoresPpales) {
 		super(name, dateTime, minutes);
-		// TODO Auto-generated constructor stub
+		this.setGenre(genre);
+		if(actoresPpales > 3)
+		{
+			System.out.println("La cantidad de actores principales no puede ser mayor a 3");
+			this.actoresPrincipales = 0;
+		}
+		else 
+		{
+			this.actoresPrincipales = actoresPpales;
+		}
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	public int getActoresPpales () {
+		return actoresPrincipales;
+	}
+	
+	public void setActoresPrincipales (int actoresPpales) {
+		this.actoresPrincipales = actoresPpales;
 	}
 
 	@Override
 	protected void calculateCost() {
-		this.cost = 1350.50;
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+	    return "Nombre: " + getName() + "\n" +
+	           "Fecha y hora: " + getDateTime() + "\n" +
+	           "Duración en minutos: " + getMinutes() + "\n" +
+	           "Género: " + getGenre().getName() + "\n" +
+	           "Actores principales: " + getActoresPpales();
 	}
-
 }
