e76d8ef1622706dfc68f1aa3a326aac74fa67bf7
PabloATorres91 <83287723+PabloATorres91@users.noreply.github.com>
2023-03-30 15:34:09 -0300

    Desarrollo de clase TheaterTicket (#3)
    * Desarrollo de clase TheaterTicket
    
    * Modificaciones en clase Actor, Genre, Main (para pruebas) y TheaterTicket
    
    * Añadiendo clase Actor

--- src/entities/Main.java
+++ src/entities/Main.java
@@ -3,14 +3,39 @@ package entities;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import entities.Genre;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		SportTicket ticketDeporte = new SportTicket("campeonato barrial",LocalDateTime.now(),120,"FUTBOL","NACIONAL");
-		SportTicket ticketDeporteB = new SportTicket("amistoso",LocalDateTime.now(),60,"RUGBY","INTERNACIONAL");
-		
+		Genre Drama = new Genre ("Drama");
+        Actor actor1 = new Actor("Tom Hanks", 65, "Male");
+        Actor actor2 = new Actor("Emma Watson", 31, "Female");
+        Actor actor3 = new Actor("Robert De Niro", 78, "Male");
+
+        Actor[] actors = { actor1, actor2, actor3 };
+
+	    // Creamos una instancia de TheaterTicket
+        TheaterTicket ticket = new TheaterTicket("The Lion King", LocalDateTime.now(), 120, Drama, 3, actors);
+	    
+        System.out.println("Agregando nuevo actor.");
+        Actor nuevoActor = new Actor("Nuevo actor", 30, "Masculino"); 
+        ticket.addActor(nuevoActor);
+	    
+        System.out.println("Mostrando actores por primera vez.");
+	    ticket.printActores();
+	    
+	    System.out.println("Eliminando segundo actor.");
+	    ticket.removeActor(1);
+	    
+	    System.out.println("Mostrando nuevamente los actores");
+	    ticket.printActores();
+	    
+	    System.out.println("Detalles de la pelicula: \n" + ticket.toString());
 	}
+	
+
 
 }
--- src/entities/TheaterTicket.java
+++ src/entities/TheaterTicket.java
@@ -1,34 +1,109 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
- * Las entradas para el teatro tienen un valor fijo de 1350,50$. 
- * A su vez, los teatros tienen sus propios g�neros y actores principales.
- * Consideraremos que como m�ximo se puede tener 3 actores principales. 
- * Los g�neros de los teatros son los siguientes: drama, teatro o comedia.
- * 
- * @author tscutti
- *
+ * Las entradas para el teatro tienen un valor fijo de 1350,50$.  A su vez, los teatros tienen sus propios generos y actores principales.
+ * Consideraremos que como maximo se puede tener 3 actores principales. Los generos de los teatros son los siguientes: drama, teatro o comedia.
  */
 public class TheaterTicket extends Ticket {
 	
 	private Genre genre;
+	private int actoresPrincipales;
+	private Actor[] actores;
 
-	public TheaterTicket(String name, LocalDateTime dateTime, int minutes, Genre genre) {
+	public TheaterTicket(String name, LocalDateTime dateTime, int minutes, Genre genre, int actoresPpales, Actor[] actores) {
 		super(name, dateTime, minutes);
-		// TODO Auto-generated constructor stub
+		this.setGenre(genre);
+		if(actoresPpales > 3)
+		{
+			System.out.println("La cantidad de actores principales no puede ser mayor a 3");
+			this.actoresPrincipales = 0;
+		}
+		else 
+		{
+			this.actoresPrincipales = actoresPpales;
+		}
+		
+	    this.actores = actores;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	public int getActoresPpales () {
+		return actoresPrincipales;
+	}
+	
+	public void setActoresPrincipales (int actoresPpales) {
+		this.actoresPrincipales = actoresPpales;
 	}
 
 	@Override
 	protected void calculateCost() {
-		this.cost = 1350.50;
+		this.cost = 1350.50;	
 	}
 
+	public Actor[] getActores() {
+		return actores;
+	}
+
+	public void setActores(Actor[] actores) {
+		this.actores = actores;
+	}
+	
+	/*
+	 * Agregar actores
+	 */
+	public void addActor(Actor actor) {
+		if (this.actores == null) {
+			this.actores = new Actor[1];
+			this.actores[0] = actor;
+		} else {
+			Actor[] newActores = Arrays.copyOf(this.actores, this.actores.length + 1);
+			newActores[newActores.length - 1] = actor;
+			this.actores = newActores;
+		}
+	}
+	
+	/*
+	 * Eliminar actores
+	 */	
+	public void removeActor(int index) {
+		if (this.actores == null || index < 0 || index >= this.actores.length) {
+			return;
+		}
+		Actor[] newActores = new Actor[this.actores.length - 1];
+		System.arraycopy(this.actores, 0, newActores, 0, index);
+		if (index < this.actores.length - 1) {
+			System.arraycopy(this.actores, index + 1, newActores, index, this.actores.length - index - 1);
+		}
+		this.actores = newActores;
+	}
+	
+	/*
+	 * Mostrar actores
+	 */	
+	public void printActores() {
+	    System.out.println("Actores:");
+	    for (int i = 0; i < actores.length; i++) {
+	        System.out.println("Actor " + (i+1) + ".");
+	        System.out.println(actores[i].toString());
+	    }
+	}
+	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		String actorsString = "";
+		for (int i = 0; i < actores.length; i++) {
+		    actorsString += "\nActor " + (i + 1) + ".\n" + actores[i].toString();
+		}
+        return super.toString() + "\nGenero: " + this.getGenre().toString()+ "\nCantidad de Actores principales: " + this.getActoresPpales()+ "\nActores: " + actorsString;
 	}
-
 }
