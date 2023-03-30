8615675ae8d5a9d900dca7b886949b60aaf289aa
Pablo Torres <p.torres0991@gmail.com>
2023-03-29 23:12:24 -0300

    Añadiendo clase Actor

---
+++ src/entities/Actor.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Actor {
+	private String name;
+	private int age;
+	private String gender;
+	
+	  public Actor(String name, int age, String gender) {
+		    this.setName(name);
+		    this.setAge(age);
+		    this.setGender(gender);
+		  }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	  
+	public String toString() {
+	    return "Nombre: " + name + "\nEdad: " + age + "\nGénero: " + gender + "\n";
+	}
+	
+}
