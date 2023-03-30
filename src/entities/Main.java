0dd0d13d2e220a132d2554e8ced03fc0f67c3605
mledesma3 <81937418+mledesma3@users.noreply.github.com>
2023-03-29 21:27:31 -0300

    SportsTicket class + calculateCosts + SportsType Interface created (#4)
    * SportsTicket class + calculateCosts + SportsType Interface created
    
    * Adjustments for SportsType - deleted interface

--- src/entities/Main.java
+++ src/entities/Main.java
@@ -1,9 +1,16 @@
 package entities;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SportTicket ticketDeporte = new SportTicket("campeonato barrial",LocalDateTime.now(),120,"FUTBOL","NACIONAL");
+		SportTicket ticketDeporteB = new SportTicket("amistoso",LocalDateTime.now(),60,"RUGBY","INTERNACIONAL");
+		
 	}
 
 }
--- src/entities/SportTicket.java
+++ src/entities/SportTicket.java
@@ -3,42 +3,86 @@ package entities;
 import java.time.LocalDateTime;
 
 /**
- * Las entradas de los eventos de deportes tienen un valor fijo según el tipo de deporte, el futbol tiene un valor fijo de 300$,
+ * Las entradas de los eventos de deportes tienen un valor fijo segï¿½n el tipo de deporte, el futbol tiene un valor fijo de 300$,
  * el rugby tiene un valor fijo de 450$ y hockey tiene un valor fijo de 380$. El deporte puede ser clasificado de manera nacional o internacional.
- * En el caso de que sea un deporte internacional, el valor de la entrada será con un recargo de un 30%.
+ * En el caso de que sea un deporte internacional, el valor de la entrada serï¿½ con un recargo de un 30%.
  * 
  * @author tscutti
  *
  */
 public class SportTicket extends Ticket implements ICategorizable {
 
-	public SportTicket(String name, LocalDateTime dateTime, int minutes) {
+	private ICategorizable.Categories category;
+	public SportsTypes sportstype;
+
+	public enum SportsTypes {
+		RUGBY,
+		FUTBOL,
+		HOCKEY
+	};
+	
+	public SportTicket(String name, LocalDateTime dateTime, int minutes, String sportstype, String category) {
 		super(name, dateTime, minutes);
-		// TODO Auto-generated constructor stub
+		this.setSportsType(sportstype);
+		this.setCategory(category);
+		this.calculateCost();
+		this.toString();
 	}
+
 
 	@Override
 	protected void calculateCost() {
-		// TODO Auto-generated method stub
+		if(SportsTypes.RUGBY == this.getSportsType()) {
+			this.cost =  450;
+		} else if (SportsTypes.FUTBOL == this.getSportsType()){
+			this.cost = 300;
+		} else if (SportsTypes.HOCKEY == this.getSportsType()) {
+			this.cost = 380;
+		}
+		else {
+			System.out.println("Tipo de deporte Inexistente");
+		}
+		if(ICategorizable.Categories.INTERNACIONAL == this.getCategory()) {
+			System.out.println("International ticket without tax  :" + this.getCost());
+			System.out.println("International tax  :" + this.getCost()*0.3);
+			this.cost = this.cost*1.3;
+		}
 		return;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		String sportsTicketInfo = "Entrada Evento deportivo \n" + super.toString();
+		sportsTicketInfo += "Costo: " + this.cost;
+		System.out.println(sportsTicketInfo);
+		return sportsTicketInfo;
 	}
 
+	
 	@Override
 	public Categories getCategory() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.category;
 	}
 
 	@Override
 	public void setCategory(String category) {
-		// TODO Auto-generated method stub
-		
+		try {
+			this.category = ICategorizable.Categories.valueOf(category);
+		} catch(IllegalArgumentException  ex) {
+			throw new IllegalArgumentException("Categoria " + category + " no disponible.");
+		}	
+	}
+	public void setSportsType(String sportstype) {
+		try {
+			this.sportstype = SportsTypes.valueOf(sportstype);
+		} catch(IllegalArgumentException  ex) {
+			throw new IllegalArgumentException("Categoria " + category + " no disponible.");
+		}	
+	}
+	
+	public SportsTypes getSportsType() {
+		return this.sportstype;
 	}
 
 }
